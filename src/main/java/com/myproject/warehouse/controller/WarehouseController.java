7fbfc880c534d4f63677d377470c5fac73df1f40
vkitori <vikibibetko@gmail.com>
2023-10-08 14:34:56 +0200

    Added product table at home page

--- src/main/java/com/myproject/warehouse/controller/WarehouseController.java
+++ src/main/java/com/myproject/warehouse/controller/WarehouseController.java
@@ -4,11 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.myproject.warehouse.domain.Product;
+import com.myproject.warehouse.service.ProductService;
+
 @Controller
 public class WarehouseController {
 	
+	private ProductService productService;
+	
+	public WarehouseController(ProductService productService) {
+		super();
+		this.productService = productService;
+	}
+
+
+
 	@GetMapping("/home")
 	public String getHome(Model model) {
+		
+		Product[] products = productService.getProducts();
+		
+		model.addAttribute("products", products);
 		
 		return "home";
 	}
